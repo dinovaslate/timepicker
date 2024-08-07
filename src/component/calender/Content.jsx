5a0930f0887188bf0dc8b6b54b540275f21a737a
Haekal Dinova <dinovasala3@gmail.com>
2024-08-07 23:39:23 +0700

    finally2

--- src/component/calender/Content.jsx
+++ src/component/calender/Content.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
-import styles from "./Calender.module.scss";
-import lodash from "lodash";
+import React, { useEffect } from 'react';
+import styles from './Calender.module.scss';
+import lodash from 'lodash';
 const Content = ({
   Year,
   Date,
@@ -10,10 +10,10 @@ const Content = ({
   handleHover,
   setDialog,
 }) => {
-  const dayOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-  const Daterange = _.range(1, Date + 1);
+  const dayOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  const Daterange = _?.range(1, Date + 1);
   return (
-    <div className={`${styles["grid-custom-calender"]}`}>
+    <div className={`${styles['grid-custom-calender']}`}>
       {dayOfWeek.map((day, index) => (
         <div className={styles.muted} key={index}>
           {day}
@@ -25,7 +25,7 @@ const Content = ({
             selectedDate.date == Date &&
             selectedDate.month == Month &&
             selectedDate.year == Year &&
-            styles["date-active"]
+            styles['date-active']
           } `}
           key={Date}
           onMouseOver={(e) => handleHover(e)}
--- src/component/calender/Reminder.jsx
+++ src/component/calender/Reminder.jsx
@@ -1,30 +1,30 @@
-import React, { Component } from "react";
-import styles from "./Calender.module.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClockFour } from "@fortawesome/free-solid-svg-icons";
-import lmao3 from "./static/lmao3.PNG";
+import React, { Component } from 'react';
+import styles from './Calender.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClockFour } from '@fortawesome/free-solid-svg-icons';
+import lmao3 from './static/lmao3.PNG';
 const Reminder = (props) => {
   return (
     <>
       <div
-        className={`${styles["d-flex-row"]} ${styles["dues"]} ${styles[status]} ${styles["align-center"]}`}
+        className={`${styles['d-flex-row']} ${styles['dues']}  ${styles['align-center']}`}
       >
         <div className={styles.imageBox}>
-          <img src={lmao3} style={{ width: "85px" }} />
+          <img src={lmao3} style={{ width: '85px' }} />
         </div>
         <div
-          className={`${styles["d-flex-column"]} ${styles["justify-around"]} ${styles.activity}`}
-          style={{ rowGap: "5px", flex: "3" }}
+          className={`${styles['d-flex-column']} ${styles['justify-around']} ${styles.activity}`}
+          style={{ rowGap: '5px', flex: '3' }}
         >
-          <div className={`${styles["activity-title"]}`}>
+          <div className={`${styles['activity-title']}`}>
             Meeting with client
           </div>
           <div
-            className={`${styles[`d-flex-row`]} ${styles["clock"]}`}
-            style={{ columnGap: "10px" }}
+            className={`${styles[`d-flex-row`]} ${styles['clock']}`}
+            style={{ columnGap: '10px' }}
           >
-            <FontAwesomeIcon icon={faClockFour} style={{ flex: "auto" }} />
-            <div style={{ flex: "auto" }}>09:00 - 10:00</div>
+            <FontAwesomeIcon icon={faClockFour} style={{ flex: 'auto' }} />
+            <div style={{ flex: 'auto' }}>09:00 - 10:00</div>
           </div>
         </div>
       </div>
